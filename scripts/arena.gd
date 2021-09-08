e4a1d8b91516f5d45ecd76e47ba6ad47b0b95041
Rui Franco <ruifr5@hotmail.com>
2021-09-08 07:13:17 +0100

    improvements to unit pathing

--- scripts/arena.gd
+++ scripts/arena.gd
@@ -3,8 +3,9 @@ extends Node2D
 export (int) var width
 export (int) var height
 export (int) var grid_width
-export (float) var unit_speed = 0.4
+export (float) var unit_speed = 1
 onready var offset = width / grid_width
+var unit_half_size
 
 #[[unit, target], ...]
 var moving_units = []
@@ -15,16 +16,34 @@ var possible_units = {
 	earth = preload("res://scenes/units/earth_unit.tscn"),
 }
 
+# normal -> normal movement
+# safe_distance -> moving away from allies
+# correction -> moving into play area
+enum MoveType { NORMAL, SAFE_DISTANCE, CORRECTION}
+
 signal unit_collision(unit1, unit2)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var unit = possible_units.fire.instance()
+	unit_half_size = unit.get_node("Sprite").get_texture().get_size().x / 2
+	unit.queue_free()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	move_all_units()
+#	touch_input()
+
+
+func touch_input():
+	if Input.is_action_just_pressed("ui_touch"):
+#		debug
+		spawn_unit_debug(get_local_mouse_position(), Vector2.UP, "fire")
+	if Input.is_action_just_pressed("ui_touch_2"):
+#		debug
+		spawn_unit_debug(get_local_mouse_position(), Vector2.DOWN, "fire")
 
 
 func spawn_unit(grid_position_x, run_direction, color):
@@ -33,6 +52,16 @@ func spawn_unit(grid_position_x, run_direction, color):
 	var unit = possible_units[color].instance()
 	unit.allegiance = run_direction
 	unit.position = calc_unit_spawn_position(unit, grid_position_x, run_direction)
+	add_child(unit)
+	moving_units.append([unit, calc_target(unit, run_direction)])
+
+
+func spawn_unit_debug(mouse_position, run_direction, color):
+	if !possible_units.has(color):
+		return
+	var unit = possible_units[color].instance()
+	unit.allegiance = run_direction
+	unit.position = mouse_position
 	add_child(unit)
 	moving_units.append([unit, calc_target(unit, run_direction)])
 
@@ -49,29 +78,51 @@ func clamp_vector(vector):
 
 
 func calc_target(unit, direction: Vector2):
-	if direction == Vector2.UP:
-		return Vector2(unit.position.x, 0)
-	if direction == Vector2.DOWN:
-		return Vector2(unit.position.x, height)
+#	check if unit is outside of play area
+	var correction = Vector2(0,0)
+	if unit.position.x < offset / 6:
+		correction.x = offset * grid_width / 2
+	elif unit.position.x > width - offset / 6:
+		correction.x = -offset * grid_width / 2
+	
+	if direction.normalized() == Vector2.UP:
+		var y = -offset if !correction.length() > 0 else unit.position.y
+		return Vector2(unit.position.x, y) + correction
+	if direction.normalized() == Vector2.DOWN:
+		var y = height + offset if !correction.length() > 0 else unit.position.y
+		return Vector2(unit.position.x, y) + correction
 
 
 func calc_unit_spawn_position(unit, grid_position_x, run_direction) -> Vector2:
-	var unit_half_size = unit.get_node("Sprite").get_texture().get_size().x / 2
 	var y = height - unit_half_size if run_direction == Vector2.UP else unit_half_size 
 	return Vector2(grid_position_x * offset + unit_half_size, y)
 
 
-func move_unit(unit: KinematicBody2D, target: Vector2):
+func move_unit(unit: KinematicBody2D, target: Vector2, move_type = MoveType.NORMAL):
 	var direction = target - unit.position
-	if unit.position.y > 0 && unit.position.y < height:
-		var collision = unit.move_and_collide(direction.normalized() * unit_speed)
-		if collision && unit.allegiance != collision.collider.allegiance:
-			emit_signal("unit_collision", unit, collision.collider)
-	else:
+	var speed = unit_speed * 10 if move_type == MoveType.CORRECTION else unit_speed
+	if unit.position.y < unit_half_size && direction.normalized() == Vector2.UP || unit.position.y > height - unit_half_size && direction.normalized() == Vector2.DOWN:
 		remove_unit(unit)
 		unit.die()
+	else:
+		var collision = unit.move_and_collide(direction.normalized() * speed)
+		if collision && unit.allegiance != collision.collider.allegiance:
+			emit_signal("unit_collision", unit, collision.collider)
+		elif collision && move_type != MoveType.SAFE_DISTANCE:
+			safe_distance(unit, collision.collider)
+#	else:
+#		remove_unit(unit)
+#		unit.die()
 
 
 func move_all_units():
 	for move_info in moving_units:
-		move_unit(move_info[0], move_info[1])
+		var unit = move_info[0]
+		var target = calc_target(unit, unit.allegiance)
+		var move_type = MoveType.CORRECTION if target.y == unit.position.y else MoveType.NORMAL
+		move_unit(unit, target, move_type)
+
+
+func safe_distance(unit1, unit2):
+	move_unit(unit1, clamp_vector(unit1.position - unit2.position)*2, MoveType.SAFE_DISTANCE)
+	move_unit(unit2, clamp_vector(unit2.position - unit1.position)*2, MoveType.SAFE_DISTANCE)
